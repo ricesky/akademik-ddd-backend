94cd6894d645e8d225e6c928cae12bc668b1e777
Rizky Januar Akbar <rja@its.ac.id>
2023-05-13 10:10:32 +0700

    test: add mode urutan pertemuan test

---
+++ presensi/domain/value_object/urutan_pertemuan.go
@@ -0,0 +1,24 @@
+package value_object
+
+import "errors"
+
+type UrutanPertemuan struct {
+	urutan int
+}
+
+func NewUrutanPertemuan(urutan int) (UrutanPertemuan, error) {
+
+	if urutan <= 0 {
+		return UrutanPertemuan{0}, errors.New("urutan_tidak_boleh_kurang_dari_atau_sama_dengan_0")
+	}
+
+	return UrutanPertemuan{urutan}, nil
+}
+
+func (u UrutanPertemuan) Urutan() int {
+	return u.urutan
+}
+
+func (u UrutanPertemuan) Equals(o UrutanPertemuan) bool {
+	return u.urutan == o.urutan
+}
---
+++ presensi/domain/value_object/urutan_pertemuan_test.go
@@ -0,0 +1,52 @@
+package value_object_test
+
+import (
+	"testing"
+
+	vo "its.id/akademik/presensi/domain/value_object"
+)
+
+func Test_urutan_pertemuan_sesuai(t *testing.T) {
+
+	u, err := vo.NewUrutanPertemuan(1)
+
+	if u.Urutan() != 1 {
+		t.Fatal("urutan tidak sesuai")
+	}
+
+	if err != nil {
+		t.Fatal("error seharusnya nil")
+	}
+
+}
+
+func Test_urutan_pertemuan_tidak_boleh_sama_dengan_nol(t *testing.T) {
+
+	_, err := vo.NewUrutanPertemuan(0)
+
+	if err == nil {
+		t.Fatal("seharusnya muncul error")
+	}
+
+}
+
+func Test_urutan_pertemuan_tidak_boleh_kurang_dari_nol(t *testing.T) {
+
+	_, err := vo.NewUrutanPertemuan(-1)
+
+	if err == nil {
+		t.Fatal("seharusnya muncul error")
+	}
+
+}
+
+func Test_urutan_pertemuan_equal(t *testing.T) {
+
+	u1, _ := vo.NewUrutanPertemuan(1)
+	u2, _ := vo.NewUrutanPertemuan(1)
+
+	if !u1.Equals(u2) {
+		t.Fatal("seharusnya equal")
+	}
+
+}
