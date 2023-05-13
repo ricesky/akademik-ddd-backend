1380ff6baf87591cfaf0c0855337c740bd5f66d9
Rizky Januar Akbar <rja@its.ac.id>
2023-05-13 07:51:46 +0700

    test: add status pertemuan test

---
+++ presensi/domain/value_object/status_pertemuan.go
@@ -0,0 +1,52 @@
+package value_object
+
+const (
+	belumDimulai      = "1"
+	sedangBerlangsung = "2"
+	selesai           = "3"
+	terlewat          = "4"
+)
+
+type StatusPertemuan struct {
+	status string
+}
+
+func NewStatusPertemuanBelumDimulai() StatusPertemuan {
+	return StatusPertemuan{belumDimulai}
+}
+
+func NewStatusPertemuanSedangBerlangsung() StatusPertemuan {
+	return StatusPertemuan{sedangBerlangsung}
+}
+
+func NewStatusPertemuanSelesai() StatusPertemuan {
+	return StatusPertemuan{selesai}
+}
+
+func NewStatusPertemuanTerlewat() StatusPertemuan {
+	return StatusPertemuan{terlewat}
+}
+
+func (s StatusPertemuan) IsBelumDimulai() bool {
+	return s.status == belumDimulai
+}
+
+func (s StatusPertemuan) IsSedangBerlangsung() bool {
+	return s.status == sedangBerlangsung
+}
+
+func (s StatusPertemuan) IsSelesai() bool {
+	return s.status == selesai
+}
+
+func (s StatusPertemuan) IsTerlewat() bool {
+	return s.status == terlewat
+}
+
+func (s StatusPertemuan) Equals(o StatusPertemuan) bool {
+	return s.status == o.status
+}
+
+func (s StatusPertemuan) Status() string {
+	return s.status
+}
---
+++ presensi/domain/value_object/status_pertemuan_test.go
@@ -0,0 +1,32 @@
+package value_object_test
+
+import (
+	"testing"
+
+	vo "its.id/akademik/presensi/domain/value_object"
+)
+
+func Test_kode_status_pertemuan_sesuai(t *testing.T) {
+
+	statusBelumDimulai := vo.NewStatusPertemuanBelumDimulai()
+	statusSedangBerlangsung := vo.NewStatusPertemuanSedangBerlangsung()
+	statusSelesai := vo.NewStatusPertemuanSelesai()
+	statusTerlewat := vo.NewStatusPertemuanTerlewat()
+
+	if !statusBelumDimulai.IsBelumDimulai() {
+		t.Fatal("status belum dimulai tidak sesuai")
+	}
+
+	if !statusSedangBerlangsung.IsSedangBerlangsung() {
+		t.Fatal("status sedang berlangsung tidak sesuai")
+	}
+
+	if !statusSelesai.IsSelesai() {
+		t.Fatal("status selesai tidak sesuai")
+	}
+
+	if !statusTerlewat.IsTerlewat() {
+		t.Fatal("status terlewat tidak sesuai")
+	}
+
+}
